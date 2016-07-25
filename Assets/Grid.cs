17f20f85be7bd1ec239c6ace2be3144c9f405c98
hadouweb <kengo14@hotmail.fr>
2016-07-25 23:59:19 +0200

    l

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -5,33 +5,40 @@ public class Grid : MonoBehaviour {
 
 	public Transform player;
 	public LayerMask unwalkableMask;
-	public Vector2 gridWorldSize;
+	public Vector3 gridWorldSize;
 	public float nodeRadius;
-	Node[,] grid;
+	Node[,,] grid;
 
 	float nodeDiameter;
 	int gridSizeX;
 	int gridSizeY;
+	int gridSizeZ;
 
 	void Start() {
 		nodeDiameter = nodeRadius * 2;
 		gridSizeX = Mathf.RoundToInt (gridWorldSize.x / nodeDiameter);
 		gridSizeY = Mathf.RoundToInt (gridWorldSize.y / nodeDiameter);
+		gridSizeZ = Mathf.RoundToInt (gridWorldSize.z / nodeDiameter);
 		CreateGrid ();
 	}
 
 	void CreateGrid() {
-		grid = new Node[gridSizeX, gridSizeY];
+		grid = new Node[gridSizeX, gridSizeY, gridSizeZ];
 
 		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 -
 			Vector3.forward * gridWorldSize.y / 2;
 
+		Gizmos.color = Color.green;
+
 		for (int x = 0; x < gridSizeX; x++) {
 			for (int y = 0; y < gridSizeY; y++) {
-				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x + nodeDiameter + nodeRadius) +
-				                     Vector3.forward * (y + nodeDiameter + nodeRadius);
-				bool walkable = !(Physics.CheckSphere (worldPoint, nodeRadius, unwalkableMask));
-				grid [x, y] = new Node (walkable, worldPoint);
+				for (int z = 0; z < gridSizeZ; z++) {
+					Vector3 worldPoint = worldBottomLeft + Vector3.right * (x + nodeDiameter + nodeRadius - 1) +
+					                    Vector3.forward * (y + nodeDiameter + nodeRadius - 1) + 
+										Vector3.up * (z + nodeDiameter + nodeRadius - 1);
+					bool walkable = !(Physics.CheckSphere (worldPoint, nodeRadius, unwalkableMask));
+					grid [x, y, z] = new Node (walkable, worldPoint);
+				}
 			}
 		}
 
@@ -40,16 +47,21 @@ public class Grid : MonoBehaviour {
 	public Node NodeFromWorldPoint(Vector3 worldPosition) {
 		float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
 		float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+		float percentZ = (worldPosition.z + gridWorldSize.z / 2) / gridWorldSize.z;
 		percentX = Mathf.Clamp01 (percentX);
 		percentY = Mathf.Clamp01 (percentY);
+		percentZ = Mathf.Clamp01 (percentZ);
 
 		int x = Mathf.RoundToInt ((gridSizeX - 1) * percentX);
 		int y = Mathf.RoundToInt ((gridSizeY - 1) * percentY);
-		return grid [x, y];	
+		int z = Mathf.RoundToInt ((gridSizeZ - 1) * percentZ);
+		return grid [x, y, z];	
 	}
 
 	void OnDrawGizmos() {
-		Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+		Vector3 pos = transform.position;
+		pos.y += gridWorldSize.y / 2;
+		Gizmos.DrawWireCube(pos, new Vector3(gridWorldSize.x, gridWorldSize.y, gridWorldSize.y));
 	
 		if (grid != null) {
 			Node playerNode = NodeFromWorldPoint (player.position);
@@ -61,6 +73,12 @@ public class Grid : MonoBehaviour {
 				Gizmos.DrawCube (n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
 			}
 		}
+
+		nodeDiameter = nodeRadius * 2;
+		gridSizeX = Mathf.RoundToInt (gridWorldSize.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt (gridWorldSize.y / nodeDiameter);
+		gridSizeZ = Mathf.RoundToInt (gridWorldSize.z / nodeDiameter);
+		CreateGrid ();
 	}
 
 }
